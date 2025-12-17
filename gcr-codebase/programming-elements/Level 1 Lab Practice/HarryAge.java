3a6466cb49d44accc984a560b063fe7a6018e07f
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-17 12:37:04 +0530

    [Aayush Rathour] Created Program to find Harry's Age

---
+++ gcr-codebase/programming-elements/Level 1 Lab Practice/HarryAge.java
@@ -0,0 +1,15 @@
+public class HarryAge{
+	public static void main(String[] args){
+		// Harry Bith Year
+		int BirthYear = 2000;
+		
+		// Current Year
+		int CurrentYear = 2024;
+		
+		// Finding the Age
+		int Age = CurrentYear-BirthYear;
+		
+		System.out.println(" Harry's age in 2024 is : " + Age);
+	}
+}
+	
