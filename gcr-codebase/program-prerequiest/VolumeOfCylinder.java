b118e9e03c46d7d4940f9518cfaedfa9a21d115c
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-17 15:49:46 +0530

    [Aayush Rathour] Created Program to find Area of Cylinder

---
+++ gcr-codebase/program-prerequiest/VolumeOfCylinder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class VolumeOfCylinder{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the radius Value");
+		int radius = sc.nextInt();
+		System.out.println("Enter the Height");
+		int height = sc.nextInt();
+		double Volume = 3.14 * radius*radius * height;
+		System.out.println(Volume);
+		
+	}
+}
