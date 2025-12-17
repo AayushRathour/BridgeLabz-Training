819cc044980006f8804e1e13c3998b2c5ab06473
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-17 16:10:42 +0530

    [Aayush Rathour] Created Program to find Average Persentage of Sam

---
+++ gcr-codebase/programming-elements/Level-1/AveragePesentage.java
@@ -0,0 +1,16 @@
+public class AveragePesentage{
+	public static void main(String[] args){
+		// Sam's Maths Marks
+		int math = 94;
+		// Sam's Physics Marks
+		int phy = 95;
+		//Sam's Chemistry Marks 
+		int chem = 96;
+		// Aveerage of All Marks 
+		int Average = (math+phy+chem)/3; 
+		System.out.println("Sam’s average mark in PCM is : " + Average + "%");
+		
+		
+		
+	}
+}
