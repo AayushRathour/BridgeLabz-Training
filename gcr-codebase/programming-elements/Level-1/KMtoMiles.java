e616950e45603af175e29bc9c58c4877a5506e04
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-17 16:29:33 +0530

    [Aayush Rathour] Created a Program to Convert KM to Miles

---
+++ gcr-codebase/programming-elements/Level-1/KMtoMiles.java
@@ -0,0 +1,12 @@
+public class KMtoMiles{
+	public static void main(String[] args){
+		// Distance in KM 
+		double KM = 10.8;
+		// 
+	 
+		System.out.println("The distance " + KM + " km in miles is : " + (KM *1.6));
+		
+		
+		
+	}
+}
