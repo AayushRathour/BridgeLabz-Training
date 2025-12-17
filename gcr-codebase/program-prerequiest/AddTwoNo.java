58fd0414d674b21f9e63f64a62801e44c6b59d2c
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-17 12:53:38 +0530

    [Aayush Rathour] Created Program to add two numbers

---
+++ gcr-codebase/program-prerequiest/AddTwoNo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddTwoNo{
+
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First number");
+		int one = sc.nextInt();
+		System.out.println("Enter the Second Number");
+		int sec =sc.nextInt();
+		System.out.println("The Output is");
+		System.out.println(one+sec);
+	}
+}
