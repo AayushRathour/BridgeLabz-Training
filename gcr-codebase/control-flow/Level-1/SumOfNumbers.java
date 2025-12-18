ce9e10970bcf3f21a685741c221befa1c72f414c
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-18 15:53:12 +0530

    [Aayush Rathour] Created a Program to add Number Untill 0 entered by user

--- gcr-codebase/control-flow/Level-1/SumOfNumbers.java
+++ gcr-codebase/control-flow/Level-1/SumOfNumbers.java
@@ -1,23 +1,22 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class SumOfNumbers {
-    public static void main(String[] args) {
-
-        // creating scanner object and initializing the total variable
-        Scanner sc = new Scanner(System.in);
-        double total = 0;
-
-        // using while loop for taking input and sum also using break to break the loop
-        while (true) {
-          
-            double input = sc.nextDouble();  
-            if (input <= 0) break;
-            total += input;
-        }
-
-        // printing the output
-        System.out.println(total);
-
-        sc.close();
-    }
+public class SumOfNumbers{
+	public static void main(String[] args){
+		// creating scanner object and taking input
+		Scanner sc = new Scanner(System.in);
+		double input = sc.nextInt();;
+		// initilizing the sum varaible
+		double total = 0;
+		
+		// using while loop for taking input and sum
+		while(input != 0){
+			total += input;
+			input = sc.nextInt();
+		}
+		
+		// printing the output
+		System.out.println(total);
+		
+		sc.close();
+	}
 }
