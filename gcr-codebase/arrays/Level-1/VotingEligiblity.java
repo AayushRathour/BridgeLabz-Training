5f5ea0c7f1bdda085dbdb7e49d917f47ec85a899
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-19 14:32:48 +0530

    [Aayush Rathour] Created a Program to check voting eligibility using Array

---
+++ gcr-codebase/arrays/Level-1/VotingEligiblity.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class VotingEligiblity{
+    public static void main(String[] args) {
+		// Creating scanner object and taking input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int number = sc.nextInt();
+       
+
+        // Printing the result
+        System.out.println("Number of digits are " + digits);
+
+    }
+}
