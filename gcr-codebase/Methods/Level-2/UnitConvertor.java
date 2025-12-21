57121fe0ef7f3547946827044495382f11a6dd68
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-21 16:34:20 +0530

    [Aayush Rathour] Created a Program for converting units and added Comments

--- gcr-codebase/Methods/Level-2/UnitConvertor.java
+++ gcr-codebase/Methods/Level-2/UnitConvertor.java
@@ -4,14 +4,15 @@ public class UnitConvertor{
 	public static double convertKmToMiles(double km){
 		return km*0.621371;
 	}
+	//Method to convert miles to kilometers and return the value
 	public static double milesTOKM(double km){
 		return miles*1.60934;
 	}
-	
+	// Method to convert meters to feet and return the value.
 	public static double meterToFeet(double km){
 		return meter*3.28084;
 	}
-	
+	// Method to convert feet to meters and return the value.
 	public static double feetToMeter(double km){
 		return feet*0.0.3048;
 	}
