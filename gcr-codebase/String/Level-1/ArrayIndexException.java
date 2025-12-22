545661f3ec75d7247d7264a3c21902bee18f69c7
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-22 14:32:49 +0530

    [Aayush Rathour] Create a program to show Array index Exception

---
+++ gcr-codebase/String/Level-1/ArrayIndexException.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ArrayIndexException{
+	//Created  method to generate array index out of bound
+	public static void generateException(String[] names) throws ArrayIndexOutOfBoundsException{
+		String str = names[names.length];
+	}
+	
+	public static void main(String[] args){
+		// creating scanner object and taking input
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of array : ");
+		int n = sc.nextInt();
+		String[] names = new String[n];
+		System.out.print("Enter names : ");
+		for(int i = 0; i < n; i++) names[i] = sc.next();
+		
+		try{
+			generateException(names);
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Exception Handeled");
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+}
