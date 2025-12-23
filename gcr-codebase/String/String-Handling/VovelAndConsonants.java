b7f7aa00f1349d0a283504282fd544721aba0e1f
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-23 16:12:44 +0530

    [Aayush Rathour] Create a program for finding vovels and Consonents(Added Comments)

--- gcr-codebase/String/String-Handling/VovelAndConsonants.java
+++ gcr-codebase/String/String-Handling/VovelAndConsonants.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-public class VowelAndConsonants {
-
+public class VowelAndConsonants{
+// Created a method 
     public static String counting(String str) {
         int vowel = 0;
         int consonant = 0;
 
         str = str.toLowerCase();
-
+// Used Loop for 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
@@ -22,12 +22,12 @@ public class VowelAndConsonants {
 
         return "Vowels = " + vowel + ", Consonants = " + consonant;
     }
-
+// creating scanner object and taking input
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Text");
         String str = sc.nextLine();
-
+// printing the output 
         System.out.println(counting(str));
     }
 }
