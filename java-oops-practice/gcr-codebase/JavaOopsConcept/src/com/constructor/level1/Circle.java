f3715bd8eb3dcb32126bf9264c7b2901f0d7e01f
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-27 17:14:37 +0530

    [Aayush Rathour] Creater a Programe for Circle

--- java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/Circle.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/Circle.java
@@ -1,5 +1,20 @@
 package com.constructor.level1;
 
 public class Circle {
+    double radius;
 
+    Circle() {
+        this(0);
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(10);
+        System.out.println(circle1.radius);
+
+       
+    }
 }
