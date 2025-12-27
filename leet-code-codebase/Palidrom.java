218c069bc4eb62c774088e39fa86e9ec223b8942
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-27 12:16:21 +0530

    [Aayush Rathour] Creater a Programe for Palidrom

---
+++ leet-code-codebase/Palidrom.java
@@ -0,0 +1,18 @@
+//9. Palindrome Number
+
+class Palidrom{
+    public boolean isPalindrome(int x) {
+        
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+}
