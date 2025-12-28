909fabd038b6fd5a2ee6d26f8715e74122959969
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-28 21:16:38 +0530

    [Aayush Rathour] Creater a Programe for Person

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/Person.java
@@ -0,0 +1,26 @@
+package com.constructor.level1;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(Person person) {
+        if (person != null) {
+            this.name = person.name;
+            this.age = person.age;
+        }
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Name1", 20);
+        Person p2 = new Person(p1);
+
+        System.out.println(p2.name);
+        System.out.println(p2.age);
+    }
+}
