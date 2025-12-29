2fb2d5ccb16e790c2a2458723ba0cdd281630a80
Aayush Rathour <aayushrathour34@gmail.com>
2025-12-29 11:24:16 +0530

    [Aayush Rathour] Creater a Programe for checking valid Parantheses

---
+++ leet-code-codebase/ValidParentheses.java
@@ -0,0 +1,26 @@
+//20. Valid Parentheses
+
+import java.util.Stack;
+
+class  ValidParentheses{
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
