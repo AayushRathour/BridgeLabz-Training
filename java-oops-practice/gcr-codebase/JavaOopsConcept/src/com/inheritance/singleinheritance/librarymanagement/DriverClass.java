a9230c2c9c4fd61e83a8d543ddc02314d76821df
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-01 16:10:33 +0530

    [Aayush Rathour] Created a Package and program for Library Management with Books and Authors

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/singleinheritance/librarymanagement/DriverClass.java
@@ -0,0 +1,8 @@
+package com.inheritance.singleinheritance.librarymanagement;
+
+public class DriverClass {
+	public static void main(String[] args) {
+		Author author = new Author("The Jungle ", 1997, "Robert", "The author is robert");
+		author.display();
+	}
+}
