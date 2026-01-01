14e5524945d30bc67dc866b86498e6e25c17822b
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-01 12:00:44 +0530

    [Aayush Rathour] Created a Package and program for University management System

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectmodelling/level1/UniversityManagementSystem/Course.java
@@ -0,0 +1,29 @@
+package com.objectmodelling.level1.UniversityManagementSystem;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String name;
+    private Professor professor;
+    private ArrayList<Student> students;
+
+    public Course(String name) {
+        this.name = name;
+        this.students = new ArrayList<>();
+    }
+
+    // method to assign professor to course
+    public void assignProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    // method to add student in course
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    // method to return course name
+    public String getCourseName() {
+        return name;
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/objectmodelling/level1/UniversityManagementSystem/Professor.java
@@ -0,0 +1,17 @@
+package com.objectmodelling.level1.UniversityManagementSystem;
+
+public class Professor {
+    private int id;
+    private String name;
+
+    // constructor
+    public Professor(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // return name
+    public String getProfessorName() {
+        return name;
+    }
+}
