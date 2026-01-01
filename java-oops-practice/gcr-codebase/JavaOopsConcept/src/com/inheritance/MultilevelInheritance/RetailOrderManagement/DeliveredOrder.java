17438ba2f434d120cdabe812cb6535bfa019d66c
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-01 16:25:57 +0530

    [Aayush Rathour] Created a Package and program for Online Retail Order Management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/MultilevelInheritance/RetailOrderManagement/DeliveredOrder.java
@@ -0,0 +1,16 @@
+package com.inheritance.MultilevelInheritance.RetailOrderManagement;
+
+public class DeliveredOrder extends ShippedOrder {
+    public String deliveryDate;
+
+    // constructor
+    public DeliveredOrder(int orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    // get status
+    public String getOrderStatus() {
+        return "Order Delivered";
+    }
+}
