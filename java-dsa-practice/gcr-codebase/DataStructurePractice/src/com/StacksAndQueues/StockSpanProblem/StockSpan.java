f2fb791d1835c03f1f75e8efb1c8ee293cb08df4
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-04 22:29:55 +0530

    [Aayush Rathour] Created a program for Stock Span Problem

---
+++ java-dsa-practice/gcr-codebase/DataStructurePractice/src/com/StacksAndQueues/StockSpanProblem/StockSpan.java
@@ -0,0 +1,32 @@
+package com.StacksAndQueues.StockSpanProblem;
+
+import java.util.*;
+public class StockSpan {
+	
+	    public static void main(String[] args) {
+
+	        int[] price = {100, 80, 60, 70, 60, 75, 85};
+	        int n = price.length;
+	        int[] span = new int[n];
+
+	        Stack<Integer> st = new Stack<>();
+
+	        for (int i = 0; i < n; i++) {
+
+	            while (!st.isEmpty() && price[st.peek()] <= price[i])
+	                st.pop();
+
+	            if (st.isEmpty())
+	                span[i] = i + 1;
+	            else
+	                span[i] = i - st.peek();
+
+	            st.push(i);
+	        }
+
+	        for (int x : span)
+	            System.out.print(x + " ");
+	    }
+	
+
+}
