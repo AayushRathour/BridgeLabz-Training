f32c1af489aa57f18e71d328e3f4e1128a4f1f44
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-07 23:10:10 +0530

    [Aayush Rathour] Created a program for file reading

---
+++ java-dsa-practice/gcr-codebase/DataStructurePractice/src/com/SearchingAlgorithm/FileReader/ReadFile.java
@@ -0,0 +1,21 @@
+package com.SearchingAlgorithm.FileReader;
+
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        try {
+        	BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } 
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
