7df5a7b8c0e6891f78fa81f7b76da5e4794de57e
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-12 14:51:21 +0530

    [Aayush Rathour] Created Program for BrowserBuddy – Tab History Manager (Doubly Linked List + Stack

---
+++ java-dsa-practice/scenariobased/src/com/BrowserBuddyTabHistoryManager/WebPageNode.java
@@ -0,0 +1,11 @@
+package com.BrowserBuddyTabHistoryManager;
+
+public class WebPageNode {
+	String url;
+	WebPageNode next;
+	WebPageNode prev;
+	
+	public WebPageNode(String url) {
+		this.url = url;
+	}
+}
