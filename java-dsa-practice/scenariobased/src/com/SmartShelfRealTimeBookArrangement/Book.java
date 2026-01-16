017e4261accc52db7a332782a7cec02f91c93bad
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-16 17:29:05 +0530

    [Aayush Rathour] modified Program for SmartShelf – Real-Time Book Arrangement (Insertion Sort)

--- java-dsa-practice/scenariobased/src/com/SmartShelfRealTimeBookArrangement/Book.java
+++ java-dsa-practice/scenariobased/src/com/SmartShelfRealTimeBookArrangement/Book.java
@@ -3,20 +3,25 @@ package com.SmartShelfRealTimeBookArrangement;
 import java.util.*;
 public class Book {
 	public static void main(String[] args) {
-		String[] book = {"Java", "Python", "Ruby", "CPP", "C", "Kotlin"};
-		System.out.println("Books : "+ Arrays.toString(book));
-		for(int i =0; i<book.length; i++) {
-			String key = book[i];
-			int j= i-1;
-			while(j>= 0 && book[j].compareTo(key)> 0) {
-				book[j+1] = book[j];
-				j--;
-				
-			}
-			book[j+1] =key;
-			
-		}
+
+		String[] book = {"Java", "Python", "Ruby", "CPP", "C"};
+		String newBook = "Kotlin";
+		System.out.println("Before: " + Arrays.toString(book));
+		book[book.length - 1] = newBook;
+
 		
-		System.out.println("Books Arranged in Series : "+ Arrays.toString(book));
+
+		for (int i = 1; i < book.length; i++) {
+			String key = book[i];
+			int j = i - 1;
+
+			while (j >= 0 && book[j].compareTo(key) > 0) {
+				book[j + 1] = book[j];
+				j--;
+			}
+			book[j + 1] = key;
+		}
+
+		System.out.println("Books Arranged in Series after adding  : " + Arrays.toString(book));
 	}
 }
