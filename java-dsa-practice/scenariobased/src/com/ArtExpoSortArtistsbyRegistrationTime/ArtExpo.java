20030cca1ab52312d613494c06b2125851838aa7
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-17 16:03:51 +0530

    [Aayush Rathour] modified Program for ArtExpo – Sort Artists by Registration Time (Insertion Sort) and added a loop for adding again and again

--- java-dsa-practice/scenariobased/src/com/ArtExpoSortArtistsbyRegistrationTime/ArtExpo.java
+++ java-dsa-practice/scenariobased/src/com/ArtExpoSortArtistsbyRegistrationTime/ArtExpo.java
@@ -3,31 +3,39 @@ package com.ArtExpoSortArtistsbyRegistrationTime;
 import java.util.*;
 public class ArtExpo {
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter the Artist one by one ");
-		System.out.println("To stop Entry press 0");
-		ArrayList<Integer> ent =new ArrayList<>();
-		for(int i =0; i<10000; i++) {
-			int t = sc.nextInt();
-			if(t==0) {
-				break;
-			}
-			ent.add(t);
+		
+		int n;
+		do {
 			
-		}
-		for(int i =0; i<ent.size();i++) {
-			int key = ent.get(i);
-			int j = i-1;
-			while(j>= 0 && ent.get(j)>key) {
-				ent.set(j+1, ent.get(j));
-				j--;
+			Scanner sc =new Scanner(System.in);
+			System.out.println("Enter the Artist one by one ");
+			System.out.println("To stop Entry press 0");
+			ArrayList<Integer> ent =new ArrayList<>();
+			for(int i =0; i<10000; i++) {
+				int t = sc.nextInt();
+				if(t==0) {
+					break;
+				}
+				ent.add(t);
 				
 			}
-			ent.set(j+1, key);
+			for(int i =0; i<ent.size();i++) {
+				int key = ent.get(i);
+				int j = i-1;
+				while(j>= 0 && ent.get(j)>key) {
+					ent.set(j+1, ent.get(j));
+					j--;
+					
+				}
+				ent.set(j+1, key);
+				
+			}
+			System.out.println("artists registration done, here is the full list : ");
+			System.out.println(ent);
+			System.out.println("Press 1 to add again : ");
+			n = sc.nextInt();
 			
-		}
-		System.out.println("artists registration done here is the list : ");
-		System.out.println(ent);
-	}
+		}while(n==1);
 		
+	}
 }
