3fbbea6850774a193114a726efa0a7ae55303635
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-23 14:02:24 +0530

    [Aayush Rathour] : Created a Program for University's Digital Record System

---
+++ java-dsa-practice/scenariobased/src/com/UniversityDigitalRecordSystem/Records.java
@@ -0,0 +1,15 @@
+package com.UniversityDigitalRecordSystem;
+
+import java.util.*;
+public class Records {
+	int left =0; right =
+	void insert(int val) {
+		add.data(val);
+	}
+	void delete(int val) {
+		remove.data(val);
+	}
+	void display(int  target) {
+		int 
+	}
+}
