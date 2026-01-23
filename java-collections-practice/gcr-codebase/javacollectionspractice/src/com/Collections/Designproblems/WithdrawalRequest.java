884fef71fc7faab80379916d40d40ea12ad14f80
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-24 00:29:34 +0530

    [Aayush Rathour] : Created a Program for Implementing Banking System

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/Collections/Designproblems/WithdrawalRequest.java
@@ -0,0 +1,12 @@
+package com.Collections.Designproblems;
+
+
+public class WithdrawalRequest {
+    int accountNumber;
+    int amount;
+
+   public WithdrawalRequest(int accountNumber, int amount) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+    }
+}
