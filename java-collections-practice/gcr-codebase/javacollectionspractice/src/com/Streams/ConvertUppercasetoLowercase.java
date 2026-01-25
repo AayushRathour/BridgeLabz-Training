ea4074b260462f88b95d269d26e7fa1d8d0af36f
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-25 17:26:38 +0530

    [Aayush Rathour] : Practiced a Program for Filter Streams - Convert Uppercase to Lowercase

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/Streams/ConvertUppercasetoLowercase.java
@@ -0,0 +1,38 @@
+
+package com.Streams;
+
+import java.io.*;
+
+public class ConvertUppercasetoLowercase {
+	public static void main(String[] args) {
+		File inputFile = new File("name.txt");
+		File outputFile = new File("Result.txt");
+		FileOutputStream fileOutputStream;
+		
+		if(!inputFile.exists()) {
+			System.out.println("Input file does not exist!");
+			return;
+		}
+		
+		try(BufferedReader reader = new BufferedReader(new FileReader(inputFile))){
+			fileOutputStream = new FileOutputStream(outputFile);
+			int ch;
+			
+			while((ch = reader.read()) != -1) {
+				if(ch >= 65 && ch <= 90) ch += 32;
+				fileOutputStream.write(ch);
+			}
+			System.out.println("Copied successfully!");
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			try {
+				fileOutputStream.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
