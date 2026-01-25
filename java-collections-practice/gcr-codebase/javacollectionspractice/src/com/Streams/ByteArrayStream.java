5ea752aae5a5ffe10b3e7d8cd1e73e814f9ea39c
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-25 17:09:11 +0530

    [Aayush Rathour] : Practiced a Program for ByteArray Stream - Convert Image to ByteArray

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/Streams/ByteArrayStream.java
@@ -0,0 +1,41 @@
+package com.Streams;
+
+import java.io.*;
+
+public class ByteArrayStream {
+	public static void main(String[] args) {
+
+		File input = new File("C:\\Users\\Acer\\Pictures\\Screenshots\\Screenshot 2026-01-25 165306.png");
+		File output = new File("CopiedImage.img");
+
+		if(!input.exists()) {
+			System.out.println("Input image not found!");
+			return;
+		}
+
+		try(FileInputStream fileInputStream = new FileInputStream(input)) {
+
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			byte[] buffer = new byte[4096];
+			int byteRead;
+
+			while((byteRead = fileInputStream.read(buffer)) != -1) {
+				byteArrayOutputStream.write(buffer , 0, byteRead);
+			}
+
+			byte[] imageBytes = byteArrayOutputStream.toByteArray();			
+
+			ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+			FileOutputStream fileOutputStream = new FileOutputStream(output);
+
+			while((byteRead = byteArrayInputStream.read(buffer)) != -1) {
+				fileOutputStream.write(buffer, 0, byteRead);
+			}
+
+			fileOutputStream.close();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
