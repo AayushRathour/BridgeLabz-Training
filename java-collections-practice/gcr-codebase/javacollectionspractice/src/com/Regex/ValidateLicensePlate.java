e9b1644faf1c2e6ac1f0384a82e589f2158bb873
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-27 00:19:07 +0530

    [Aayush Rathour] : Created a programe for Validate a License Plate Number

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/Regex/ValidateLicensePlate.java
@@ -0,0 +1,11 @@
+package com.Regex;
+
+public class ValidateLicensePlate {
+	private static boolean validateLicensePlateNumber(String plateNumber) {
+		return plateNumber.matches("^[A-Z]{2}[0-9]{4}$");
+	}
+	public static void main(String[] args) {
+		System.out.println(validateLicensePlateNumber("AB1234"));
+		System.out.println(validateLicensePlateNumber("A01234"));
+	}
+}
