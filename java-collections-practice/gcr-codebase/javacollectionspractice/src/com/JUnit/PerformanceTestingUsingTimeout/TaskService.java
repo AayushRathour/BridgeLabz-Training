eed5f98ffce46a30e6a3b69318f13f9e6f4bfe8d
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-27 02:04:48 +0530

    [Aayush Rathour] : Created a programe for Performance Testing Using @Timeout

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/JUnit/PerformanceTestingUsingTimeout/TaskService.java
@@ -0,0 +1,8 @@
+package com.JUnit.PerformanceTestingUsingTimeout;
+
+public class TaskService {
+    public String longRunningTask() throws InterruptedException {
+        Thread.sleep(3000);
+        return "Completed";
+    }
+}
