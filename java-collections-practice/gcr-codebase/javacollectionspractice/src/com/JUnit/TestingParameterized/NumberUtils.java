c2f8fe6854da0f45a9fb5bc419369a2a82a303ab
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-27 02:03:45 +0530

    [Aayush Rathour] : Created a programe for Testing Parameterized Tests

--- java-collections-practice/gcr-codebase/javacollectionspractice/src/com/JUnit/TestingParameterized/NumberUtils.java
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/JUnit/TestingParameterized/NumberUtils.java
@@ -1,5 +1,7 @@
 package com.JUnit.TestingParameterized;
 
-public class NumberUtils {
-
+public class NumberUtils {	
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
 }
