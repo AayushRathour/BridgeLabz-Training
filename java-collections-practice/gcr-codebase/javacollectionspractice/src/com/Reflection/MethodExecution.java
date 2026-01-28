2fc677da42f96adc9540dbaf24a5033b2487c8b2
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-28 16:41:18 +0530

    [Aayush Rathour] : Created the program for Method Execution Timing

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/Reflection/MethodExecution.java
@@ -0,0 +1,22 @@
+package com.Reflection;
+
+import java.lang.reflect.Method;
+
+public class MethodExecution {
+    static void measureExecutionTime(Object obj, String methodName) throws Exception {
+        Class<?> class1 = obj.getClass();
+        Method method = class1.getDeclaredMethod(methodName);
+        method.setAccessible(true);
+
+        long startTime = System.nanoTime();
+        method.invoke(obj);
+        long endTime = System.nanoTime();
+
+        System.out.println(endTime - startTime);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Task task = new Task();
+        measureExecutionTime(task, "method");
+    }
+}
