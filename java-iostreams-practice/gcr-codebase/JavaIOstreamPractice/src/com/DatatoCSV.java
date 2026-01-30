b3b9f28567e5e5021f0249d510e45d74653c205b
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-30 12:34:07 +0530

    [Aayush Rathour] : Created a Program to Wtite data in CSV File

--- java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/DatatoCSV.java
+++ java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/DatatoCSV.java
@@ -1,5 +1,14 @@
 package com;
 
+import java.util.*;
+import java.io.*;
 public class DatatoCSV {
-
+	public static void main(String[] args)throws Exception {
+		FileWriter fw = new FileWriter("New.csv");
+	    fw.write("ID,Name,RollNo\n");
+        fw.write("1,Aayush,001\n");
+        fw.write("2,Rahul,002\n");
+        fw.close();
+	}
+	
 }
