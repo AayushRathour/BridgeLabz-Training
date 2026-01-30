4463f80303d64a7dd0aa9b9f70a7cbd98080cebf
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-30 16:09:02 +0530

    [Aayush Rathour] : Created a Programe for Read Large CSV File Efficiently

--- java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/ReadLargeCSV.java
+++ java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/ReadLargeCSV.java
@@ -1,5 +1,34 @@
 package com;
 
-public class ReadLargeCSV {
+import java.io.*;
 
+public class ReadLargeCSV {
+    public static void main(String[] args) {
+        int batchSize = 100;
+        int batchCount = 0;
+        int totalCount = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("large.csv"))) {
+            String line;
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                batchCount++;
+                totalCount++;
+
+                if (batchCount == batchSize) {
+                    System.out.println("Processed records: " + totalCount);
+                    batchCount = 0;
+                }
+            }
+
+            if (batchCount > 0) {
+                System.out.println("Processed records: " + totalCount);
+            }
+
+        } 
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
