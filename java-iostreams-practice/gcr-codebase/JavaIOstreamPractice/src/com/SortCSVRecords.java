d822dc0f82307d019fd9252edbbdfe149b9ea43d
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-30 15:50:10 +0530

    [Aayush Rathour] : Created a Program to Sort CSV Records by a Column

---
+++ java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/SortCSVRecords.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.io.*;
+import java.util.*;
+
+public class SortCSVRecords {
+    public static void main(String[] args) {
+        List<String[]> list = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("employees.csv"))) {
+            String line;
+            br.readLine(); 
+
+            while ((line = br.readLine()) != null) {
+                list.add(line.split(","));
+            }
+
+        } 
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Collections.sort(list, (a, b) -> Integer.parseInt(b[3]) - Integer.parseInt(a[3]));
+
+        for(int i = 0; i < Math.min(5, list.size()); i++) System.out.println(Arrays.toString(list.get(i)));
+    }
+}
