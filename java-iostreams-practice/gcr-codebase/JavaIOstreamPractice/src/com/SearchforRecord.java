ab4160499d0dd7ea3391466e2483214f576e11b3
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-30 12:39:04 +0530

    [Aayush Rathour] : Created a Program to Search for a Record in CSV

---
+++ java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/SearchforRecord.java
@@ -0,0 +1,28 @@
+package com;
+
+import java.io.*;
+
+public class SearchforRecord {
+    public static void main(String[] args) {
+        String searchName = "name3";
+        try (BufferedReader br = new BufferedReader(new FileReader("employees.csv"))) {
+            String line;
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                String[] employee = line.split(",");
+
+                if (employee[1].equalsIgnoreCase(searchName)) {
+                    System.out.println(employee[0] + " " + employee[1] + " " + employee[2] + " " + employee[3]);
+                    return;
+                }
+            }
+
+            System.out.println("Employee not found");
+
+        } 
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
