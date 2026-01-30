7e765493bb8ae9aa01c3881eaad670ff34db8b2e
Aayush Rathour <aayushrathour34@gmail.com>
2026-01-30 16:20:35 +0530

    [Aayush Rathour] : Created a Programe to Generate a CSV Report from Database

--- java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/ReportfromDatabase/Employee.java
+++ java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/ReportfromDatabase/Employee.java
@@ -1,5 +1,15 @@
 package com.ReportfromDatabase;
 
 public class Employee {
+    int employeeId;
+    String name;
+    String department;
+    double salary;
 
+    public Employee(int employeeId, String name, String department, double salary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
 }
--- java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/ReportfromDatabase/EmployeeDatabase.java
+++ java-iostreams-practice/gcr-codebase/JavaIOstreamPractice/src/com/ReportfromDatabase/EmployeeDatabase.java
@@ -1,5 +1,15 @@
 package com.ReportfromDatabase;
 
-public class EmployeeDatabase {
+import java.util.ArrayList;
+import java.util.List;
 
+public class EmployeeDatabase {
+    List<Employee> employees;
+    public EmployeeDatabase() {
+        employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
 }
