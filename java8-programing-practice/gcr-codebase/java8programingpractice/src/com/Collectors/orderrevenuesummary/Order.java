2d3326de2839c0dbe543488ed0141413c2bdc46a
Aayush Rathour <aayushrathour34@gmail.com>
2026-02-02 15:51:48 +0530

    [Aayush Rathour] : Created a programe for Order Revenue Summary

---
+++ java8-programing-practice/gcr-codebase/java8programingpractice/src/com/Collectors/orderrevenuesummary/Order.java
@@ -0,0 +1,11 @@
+package com.Collectors.orderrevenuesummary;
+
+public class Order {
+    String customer;
+    double amount;
+
+    public Order(String customer, double amount) {
+        this.customer = customer;
+        this.amount = amount;
+    }
+}
