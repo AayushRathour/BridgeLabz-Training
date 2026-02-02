32fc98b9807b3ceb7bf97f2d874901fea84552e2
Aayush Rathour <aayushrathour34@gmail.com>
2026-02-02 15:49:23 +0530

    [Aayush Rathour] : Created a programe for Library Book Statistics

---
+++ java8-programing-practice/gcr-codebase/java8programingpractice/src/com/Collectors/bookstatistics/Book.java
@@ -0,0 +1,11 @@
+package com.Collectors.bookstatistics;
+
+public class Book {
+    String genre;
+    int pages;
+
+    public Book(String genre, int pages) {
+        this.genre = genre;
+        this.pages = pages;
+    }
+}
